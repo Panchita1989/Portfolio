74e987e88399651cd31439874e95fef0e7c86ec1
Panchita <franciscalandwehr@hotmail.com>
2025-05-08 15:54:34 -0500

    added back to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,3 +265,22 @@
 			});
 
 })(jQuery);
+let backToTop = document.querySelector('.backToTop')
+let scrollYPos1 = window.scrollY
+
+window.addEventListener('scroll', toTop)
+
+function toTop(){
+	let scrollYPos2 = window.scrollY
+	let dif = scrollYPos1 - scrollYPos2
+	scrollYPos1 = scrollYPos2
+
+	if(dif > 0 && scrollY > 80){
+		backToTop.classList.add('visible')
+	}else{
+		backToTop.classList.remove('visible')
+	}
+}
+backToTop.addEventListener('click', ()=>{
+	scrollTo({top:0, behavior: 'smooth'})
+})
